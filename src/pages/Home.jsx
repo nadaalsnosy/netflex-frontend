181a40274175a7962b90ccc2e80681f856067fcb
Nada Elsnosy <nadaalsnosy@gmail.com>
2022-03-17 13:44:29 +0200

    add change

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ const Home = () => {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit It<code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
