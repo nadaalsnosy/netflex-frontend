4249d6c68d7d38c1b3a3285727e941da06c2f24d
EmanYoussef799 <emanyoussef799@gmail.com>
2022-03-26 05:14:03 -0700

    edit profile

---
+++ src/components/EditProfile.jsx
@@ -0,0 +1,252 @@
+import { Form, Button } from "react-bootstrap";
+import { Link, useNavigate } from "react-router-dom";
+import { useState, useEffect, useRef } from "react";
+
+import axios from "../api/axios";
+import useAuth from "../hooks/useAuth";
+import NetflixLogo from "../components/LoggedOut/NetflixLogo";
+
+const userREGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,20}$/;
+const emailREGEX = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+const passwordREGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%^&*])/;
+
+const EditProfile = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+  const navigate = useNavigate();
+  const { auth } = useAuth();
+
+  console.log(auth);
+
+  const [userName, setUserName] = useState("");
+  const [validName, setValidName] = useState(false);
+  const [nameFocus, setNameFocus] = useState(false);
+
+  const [userEmail, setUserEmail] = useState("");
+  const [validEmail, setValidEmail] = useState(false);
+  const [emailFocus, setEmailFocus] = useState(false);
+
+  const [userPassword, setUserPassword] = useState("");
+  const [validPassword, setValidPassword] = useState(false);
+  const [passwordFocus, setPasswordFocus] = useState(false);
+
+  const [userConfirmPassword, setUserConfirmPassword] = useState("");
+  const [validConfirmPassword, setValidConfirmPassword] = useState(false);
+  const [confirmPasswordFocus, setConfirmPasswordFocus] = useState(false);
+
+  const [errMsg, setErrMsg] = useState("");
+
+  useEffect(() => {
+    userRef.current.focus();
+  }, []);
+
+  useEffect(() => {
+    const isValid = userREGEX.test(userName);
+    setValidName(isValid);
+  }, [userName]);
+
+  useEffect(() => {
+    const isValid = emailREGEX.test(userEmail);
+    setValidEmail(isValid);
+  }, [userEmail]);
+
+  useEffect(() => {
+    const isValid = passwordREGEX.test(userPassword);
+    setValidPassword(isValid);
+    const match = userPassword === userConfirmPassword;
+    setValidConfirmPassword(match);
+  }, [userPassword, userConfirmPassword]);
+
+  useEffect(() => {
+    setErrMsg("");
+  }, [userName, userEmail, userPassword, userConfirmPassword]);
+
+  const handelSubmit = async (e) => {
+    e.preventDefault();
+    const validName = userREGEX.test(userName);
+    const validEmail = emailREGEX.test(userEmail);
+    const validPassword = passwordREGEX.test(userPassword);
+    if (!validName || !validEmail || !validPassword) {
+      setErrMsg("Invalid data");
+      return;
+    }
+    try {
+      const res = await axios.post(
+        "/users/signUp",
+        JSON.stringify({
+          username: userName,
+          email: userEmail,
+          password: userPassword,
+        }),
+        {
+          headers: { "content-type": "application/json" },
+        }
+      );
+      console.log(res);
+
+      navigate("/signIn");
+    } catch (err) {
+      if (!err?.response) {
+        console.log("No Server Response");
+      } else if (err.response?.status === 400) {
+        setErrMsg("Email is already exist");
+      } else {
+        console.log("Regiseration Faild");
+      }
+      console.log(err);
+    }
+  };
+  return (
+    <>
+      <div className="">
+        <div className="overlay">
+          <NetflixLogo />
+          <div className="pb-4">
+            <div>
+              <Form
+                className="signForm text-white bg-black-8"
+                onSubmit={handelSubmit}
+              >
+                <h1 className=" mb-5 fw-bold ">Edit Profile</h1>
+                <Form.Group className="mb-4" controlId="formGridName">
+                  <Form.Control
+                    className={`bg-gray h-50p border-0 ${
+                      nameFocus && !validName ? "errInput" : ""
+                    } `}
+                    type="text"
+                    placeholder="Enter name"
+                    ref={userRef}
+                    value={userName}
+                    onChange={(e) => setUserName(e.target.value)}
+                    required
+                    aria-describedby="userName"
+                    onFocus={() => setNameFocus(true)}
+                  />
+                  <p
+                    id="userName"
+                    className={`errMsg ${
+                      nameFocus && !validName ? "shown" : "hidden"
+                    }`}
+                  >
+                    please enter valid name!
+                  </p>
+                </Form.Group>
+
+                <Form.Group className="mb-4" controlId="formGridEmail">
+                  <Form.Control
+                    className={`bg-gray h-50p border-0 ${
+                      (emailFocus && !validEmail) || errMsg ? "errInput" : ""
+                    }`}
+                    type="email"
+                    placeholder="Enter Email"
+                    ref={userRef}
+                    value={userEmail}
+                    onChange={(e) => setUserEmail(e.target.value)}
+                    required
+                    aria-invalid={validEmail ? false : true}
+                    aria-describedby="userEmail"
+                    onFocus={() => setEmailFocus(true)}
+                  />
+                  <p
+                    id="userEmail"
+                    className={`errMsg ${
+                      emailFocus && !validEmail ? "shown" : "hidden"
+                    } ${errMsg ? "none" : "block"}`}
+                  >
+                    please enter valid email!
+                  </p>
+                  <p
+                    ref={errRef}
+                    className={`errMsg ${errMsg ? "shown" : "hidden"} ${
+                      emailFocus && !validEmail ? "none" : "block"
+                    }`}
+                    aria-live="assertive"
+                  >
+                    {errMsg}
+                  </p>
+                </Form.Group>
+
+                <Form.Group className="mb-4" controlId="formGridPassword">
+                  <Form.Control
+                    className={`bg-gray h-50p border-0 ${
+                      passwordFocus && !validPassword ? "errInput" : ""
+                    }`}
+                    type="password"
+                    placeholder="Enter Password"
+                    ref={userRef}
+                    value={userPassword}
+                    onChange={(e) => setUserPassword(e.target.value)}
+                    required
+                    aria-describedby="userPassword"
+                    onFocus={() => setPasswordFocus(true)}
+                  />
+                  <p
+                    id="userPassword"
+                    className={`errMsg ${
+                      passwordFocus && !validPassword ? "shown" : "hidden"
+                    }`}
+                  >
+                    please enter valid password!
+                  </p>
+                </Form.Group>
+
+                <Form.Group
+                  className="mb-4"
+                  controlId="formGridConfirmPassword"
+                >
+                  <Form.Control
+                    className={`bg-gray h-50p border-0 ${
+                      confirmPasswordFocus && !validConfirmPassword
+                        ? "errInput"
+                        : ""
+                    }`}
+                    type="password"
+                    placeholder="Enter ConfirmPassword"
+                    ref={userRef}
+                    value={userConfirmPassword}
+                    onChange={(e) => setUserConfirmPassword(e.target.value)}
+                    required
+                    aria-describedby="userConfirmPassword"
+                    onFocus={() => setConfirmPasswordFocus(true)}
+                  />
+                  <p
+                    id="userConfirmPassword"
+                    className={`errMsg ${
+                      confirmPasswordFocus && !validConfirmPassword
+                        ? "shown"
+                        : "hidden"
+                    }`}
+                  >
+                    Must match your password input!
+                  </p>
+                </Form.Group>
+
+                <div className="text-end mt-5">
+                  <Button
+                    variant="danger w-100 h-50p fs-5"
+                    type="submit"
+                    disabled={
+                      !validName ||
+                      !validEmail ||
+                      !validPassword ||
+                      !validConfirmPassword
+                        ? true
+                        : false
+                    }
+                  >
+                    Save
+                  </Button>
+                </div>
+
+               
+              </Form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+
+export default EditProfile;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,56 @@
+import { Form, Button } from "react-bootstrap";
+import AvatarImage from "../images/avatar.png";
+import {Link} from "react-router-dom";
+// import useAuth from "../hooks/useAuth";
+
 const Profile = () => {
+  // const { auth } = useAuth();
   return (
-    <div className="mt-5 pt-4">
-      <h1 className="text-white">Profile</h1>
-    </div>
+    <>
+      <div className="">
+        <div className="overlay">
+          <div className="pb-1 pt-1">
+            <Form className="signForm text-white bg-black-8">
+              <h1 className=" mb-5 fw-bold ">Profile</h1>
+
+              <Form.Group className="mb-4" controlId="formGridEmail">
+                <div>
+                  <img
+                    className="avatarPhoto"
+                    src={AvatarImage}
+          
+                    alt="avatar"
+                  />
+                </div>
+                 <div>User name : auth.user.name</div>
+                <div>Email : auth.user.email</div>
+                <div>Password : ********</div>
+                {/* <div>User name : {auth.user.name}</div>
+                <div>Email : {auth.user.email}</div>
+                <div>Password : ********</div> */}
+              </Form.Group>
+
+              <Form.Group
+                className="mb-4"
+                controlId="formGridPassword"
+              ></Form.Group>
+
+              <div className="text-end  ">
+               
+                <Link
+                    className="text-primary text-decoration-none"
+                    to={`/EditProfile`}
+                  >
+                   <Button variant="danger w-100 h-50p mb-3" type="submit"> Edit </Button>
+                  </Link> 
+                
+                
+              </div>
+            </Form>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
