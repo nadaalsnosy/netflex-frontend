a1d81674b946cd2032f0588207a015e77f42357e
EmanYoussef799 <emanyoussef799@gmail.com>
2022-03-27 17:46:42 -0700

    my list

--- src/components/Login/MovieCard.jsx
+++ src/components/Login/MovieCard.jsx
@@ -1,4 +1,4 @@
-import { Card } from "react-bootstrap";
+import{ Card } from "react-bootstrap";
 import {
   PlayArrow,
   ThumbDownOffAlt,
@@ -14,14 +14,14 @@ const MovieCard = (props) => {
   const { item } = props;
 
   return (
-    <Link to={`/mainVideo/${item._id}`}>
+    // <Link to={`/mainVideo/${item._id}`}>
       <Card
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
         className="cardItem d-block"
       >
-        <Card.Img variant="top" src={item.img} />
-        {isHovered && <video autoPlay muted loop src={item.trailer}></video>}
+        <Card.Img variant="top" src="{item.img}" />
+        {isHovered && <video autoPlay muted loop src="{item.trailer}"></video>}
         <Card.Body className="pb-1 bg-black">
           <Card.Title className="d-flex justify-content-between mb-4">
             <span className="cardIcon playArrowIcon">
@@ -38,15 +38,15 @@ const MovieCard = (props) => {
             </span>
           </Card.Title>
           <Card.Text className="d-flex justify-content-between align-items-center mb-3">
-            <span className="text-lightGreen fs-6">Rate {item.rate}</span>
-            <span className="cardBoder fs-8">{item.limit}</span>
+            {/* <span className="text-lightGreen fs-6">Rate "{item.rate}"</span>
+            <span className="cardBoder fs-8">"{item.limit}"</span> */}
             <span className="fs-6"> 1h 38m</span>
             <span className="cardBoder fs-8"> HD</span>
           </Card.Text>
-          <Card.Text className="mb-1">{item.genere}</Card.Text>
+          {/* <Card.Text className="mb-1">"{item.genere}"</Card.Text> */}
         </Card.Body>
       </Card>
-    </Link>
+    // </Link>
   );
 };
 
--- src/pages/UserList.jsx
+++ src/pages/UserList.jsx
@@ -1,9 +1,16 @@
 import React from "react";
+import MovieCard from "../components/Login/MovieCard"
 
 const UserList = () => {
   return (
     <>
-      <div className="mt-5 pt-5 text-white">UserList</div>;
+      <div className=" container mt-5 pt-5 mb-5 text-white fw-bold"><h1 className="mb-2">My List</h1>
+       <div className="d-flex mb-2 flex-wrap"><MovieCard /><MovieCard /><MovieCard />
+      <MovieCard /><MovieCard /><MovieCard /><MovieCard /><MovieCard />
+      <MovieCard /><MovieCard /><MovieCard /><MovieCard /></div>
+      </div>;
+     
+
     </>
   );
 };
